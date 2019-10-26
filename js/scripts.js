9edaad37625034ad4c13cdb9301939c9d6c63975
Marie-Ehrman <53120243+Marie-Ehrman@users.noreply.github.com>
2019-10-25 17:27:09 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -3,58 +3,36 @@
 
 //fetch data from the Random User Generator
 
-fetch('https://randomuser.me/api/')
+fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json() )
     .then(data => {
-        generateImage(data.results[0].picture.large)
-        generateUserName(data.results[0].name)
-        generateUserEmail(data.results[0].email)
-        generateUserLocation(data.results[0].location)
-    })
+            generateUserCard(data)
+            generateModalWindow(data)
+            });
 
 
-    
-//function for generating the random users photo and adding it to html
-function generateImage(data){
 
-    const galleryHTML = `
-    <img class="card-img" src="${data}" alt="profile picture">
+//dynamically add gallery html template literal and append to the gallery 
+//div using example text given in html file combined with json data
+function generateUserCard(data){
+
+    for( let i = 0; i < data.results.length; i++){
+
+        let userCardDiv = `
+        <div class="card">
+            <div class="card-img-container">
+            <img class="card-img" src="${data.results[i].picture.medium}" alt="profile picture">
+            </div>
+            <div class="card-info-container">
+            <h3 id="name" class="card-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+            <p class="card-text">${data.results[i].email}</p>
+            <p class="card-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
+            </div>
+        </div>
     `;
-
-    $('.card-img-container').append(galleryHTML);
+      $('.gallery').append(userCardDiv);
+    }
 }
-
-//function for generating the random users name 
-function generateUserName(name){
-
-    const userNameHTML = `
-    <h3 id="name" class="card-name cap">${name.first} ${name.last}</h3>
-    `;
-
-    $('.card-info-container').append(userNameHTML);
-}
-
-//function for generating the random users email 
-function generateUserEmail(email){
-
-    const userEmailHTML = `
-    <p class="card-text">${email}</p>
-    `;
-
-    $('.card-info-container').append(userEmailHTML);
-}
-
-//function for generating the random users loctaion 
-function generateUserLocation(location){
-
-    const userLocationHTML = `
-    <p class="card-text cap">${location.city}, ${location.state}</p>
-    `;
-
-    $('.card-info-container').append(userLocationHTML);
-}
-
-
 
 
 //dynamically add search bar html template literal and append to the search 
@@ -68,31 +46,33 @@ $('.search-container').append(`
 
 
 
-//dynamically add gallery html template literal and append to the gallery 
-//div using example text given in html file.
-$('.gallery').append(`
-<div class="card">
-<div class="card-img-container"></div>
-<div class="card-info-container"></div>
-</div>
-`);
 
 
+function generateModalWindow(data){
 //dynamically add modal window html template literal and append to the body
 //div using example text given in html file.
-$('.body').append(`
-<div class="modal-container">
-<div class="modal">
-    <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-    <div class="modal-info-container">
-        <img class="modal-img" src="https://placehold.it/125x125" alt="profile picture">
-        <h3 id="name" class="modal-name cap">name</h3>
-        <p class="modal-text">email</p>
-        <p class="modal-text cap">city</p>
-        <hr>
-        <p class="modal-text">(555) 555-5555</p>
-        <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-        <p class="modal-text">Birthday: 10/21/2015</p>
-    </div>
-</div>
-`);
+$('.card').click(function (e){
+    console.log(data.results);
+    console.log(e.target)
+    console.log(`${data.results.name.first} ${data.results[this].name.last}`)
+
+    
+        $('body').append(`
+        <div class="modal-container">
+        <div class="modal">
+            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+            <div class="modal-info-container">
+                <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
+                <h3 id="name" class="modal-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+                <p class="modal-text">${data.results[i].email}</p>
+                <p class="modal-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
+                <hr>
+                <p class="modal-text">(555) 555-5555</p>
+                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
+                <p class="modal-text">Birthday: 10/21/2015</p>
+            </div>
+        </div>
+        `);
+});
+
+}
