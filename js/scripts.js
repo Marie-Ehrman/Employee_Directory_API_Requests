d37df863add578d74fd21a5c8a0d81b3954e8eb5
Marie-Ehrman <53120243+Marie-Ehrman@users.noreply.github.com>
2019-10-27 15:39:51 -0700

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -46,6 +46,9 @@ $('.search-container').append(`
 
 
 
+//click box
+//get the card DOM element
+//
 
 
 function generateModalWindow(data){
@@ -53,26 +56,38 @@ function generateModalWindow(data){
 //div using example text given in html file.
 $('.card').click(function (e){
     console.log(data.results);
-    console.log(e.target)
-    console.log(`${data.results.name.first} ${data.results[this].name.last}`)
+    // console.log(`${data.results[e.currentTarget].name.first} ${data.results[this].name.last}`)
+const clickedUserName = e.currentTarget.childNodes[3].childNodes[1].textContent.toLowerCase();
+console.log(clickedUserName);
 
+    for( let i = 0; i < data.results.length; i++) {   
     
-        $('body').append(`
-        <div class="modal-container">
-        <div class="modal">
-            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-            <div class="modal-info-container">
-                <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
-                <h3 id="name" class="modal-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
-                <p class="modal-text">${data.results[i].email}</p>
-                <p class="modal-text cap">${data.results[i].location.city}, ${data.results[i].location.state}</p>
-                <hr>
-                <p class="modal-text">(555) 555-5555</p>
-                <p class="modal-text">123 Portland Ave., Portland, OR 97204</p>
-                <p class="modal-text">Birthday: 10/21/2015</p>
+    let displayUserName = `${data.results[i].name.first} ${data.results[i].name.last}`.toLowerCase();
+        if ( clickedUserName === displayUserName){
+            console.log(data.results[i].location.street);
+            console.log(displayUserName);
+
+            $('body').append(`
+            <div class="modal-container">
+            <div class="modal">
+                <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                <div class="modal-info-container">
+                    <img class="modal-img" src="${data.results[i].picture.large}" alt="profile picture">
+                    <h3 id="name" class="modal-name cap">${data.results[i].name.first} ${data.results[i].name.last}</h3>
+                    <p class="modal-text">${data.results[i].email}</p>
+                    <p class="modal-text cap">${data.results[i].location.city}</p>
+                    <hr>
+                    <p class="modal-text">${data.results[i].cell}</p>
+                    <p class="modal-text">${data.results[i].location.street.number} ${data.results[i].location.street.name}, 
+                                        ${data.results[i].location.city},
+                                        ${data.results[i].location.state}
+                                        ${data.results[i].location.postcode}</p>
+                    <p class="modal-text">Birthday: ${data.results[i].dob}</p>
+                </div>
             </div>
-        </div>
-        `);
+            `);
+        }
+    }
 });
 
 }
